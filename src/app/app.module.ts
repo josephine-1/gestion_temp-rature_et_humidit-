f4b7f6a869508ba8922f290af1b8263aadb61c3a
Josephine <sosodjibrildiop@gmail.com>
2023-01-02 18:24:34 +0000

    interface-admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageAdminComponent } from './page-admin/page-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAdminComponent
   ],
   imports: [
     BrowserModule,
