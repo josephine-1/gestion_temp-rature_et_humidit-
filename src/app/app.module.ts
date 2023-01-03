22cdba0a4f3eb08b46d5810d7365ef667402268b
zoukana <assanediallo192@gmail.com>
2023-01-03 16:58:16 +0000

    ajout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//  import { ReactiveFormsModule } from '@angular/forms'
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ModifierProfilComponent } from './modifier-profil/modifier-profil.component';
+import { ModifierPasswordComponent } from './modifier-password/modifier-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    ProfilComponent,
+    ModifierProfilComponent,
+    ModifierPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/inscription/inscription.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+//ici j'importe des proprietés de angular liées a l'utilisation des formulaire
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-inscription',
+  templateUrl: './inscription.component.html',
+  styleUrls: ['./inscription.component.scss']
+})
+export class InscriptionComponent implements OnInit{
+registerForm!: FormGroup
+submitted: Boolean= false
+//ici on gére le controle de saisit du formulaire
+ngOnInit(){
+  this.registerForm = this.formBuilder.group({
+    prenom: ['', Validators.required],
+    nom: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+    role: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+
+
+  }, {
+    validator: MustMatch('password', 'confirmPassword')//ici je précise au validator que la class MushMatch
+                                                      //va géré la comparaison des mots de passe
+});
+
+  }
+constructor(private formBuilder: FormBuilder){
+
+}
+
+   // la fonction getter est utiliser pour un accès facile aux champs de formulaire
+   get f() { return this.registerForm.controls; }
+
+   onSubmit() {
+       this.submitted = true;
+
+       // arrêtez-vous ici si le formulaire est invalide
+       if (this.registerForm.invalid) {
+           return;
+       }
+
+       alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+   }
+
+}
+//ici j'exporte la class MushMatch pour la gestion de mes mots de passes
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          //renvoie si un autre validateur a déjà trouvé une erreur sur le matchingControl
+          return;
+      }
+
+      //définir une erreur sur matchingControl si la validation échoue
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  }
+}
