29518a118f729fbe1ec3616826b4076117638dc2
Josephine <sosodjibrildiop@gmail.com>
2023-01-03 16:55:06 +0000

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { PageAdminComponent } from './page-admin/page-admin.component';
 import { ListeUserComponent } from './liste-user/liste-user.component';
 import { PageUserComponent } from './page-user/page-user.component';
+import { ListeAdministrateurComponent } from './liste-administrateur/liste-administrateur.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PageAdminComponent,
     ListeUserComponent,
-    PageUserComponent
+    PageUserComponent,
+    ListeAdministrateurComponent,
+   
+  
+   
   ],
   imports: [
     BrowserModule,
--- src/app/liste-user/liste-user.component.ts
+++ src/app/liste-user/liste-user.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
- import UsersJson from '/home/josephine/Documents/gestion_temp-rature_et_humidit-/gestion_th/src/app/users.json'; 
+import { Component,OnInit } from '@angular/core';
+import UsersJson from '/home/josephine/Documents/gestion_temp-rature_et_humidit-/gestion_th/src/app/users.json';
 
 
- interface USERS {
-  id: Number;
-  name: String;
-  username: String;
-  email: String;
+interface USERS {
+  
+  Nom: String;
+  Prenom: String;
+  Matricule: String;
+  Email: String;
+  Date_inscription: String;
 }
 
 @Component({
@@ -14,9 +16,35 @@ import { Component } from '@angular/core';
   templateUrl: './liste-user.component.html',
   styleUrls: ['./liste-user.component.scss']
 })
-export class ListeUserComponent {
+export class ListeUserComponent implements OnInit{
+  /* Users: USERS[] = UsersJson;
+  constructor() {
+    console.log(this.Users);
+  } */
+  p:number=1;
+  searchText!:string;
   Users: USERS[] = UsersJson;
+
+  prenom!:any;
+  nom!:any;
+  matricule!:any
+  etat:any = localStorage.getItem('token');
+
   constructor(){
     console.log(this.Users);
+  }
+  ngOnInit(): void {
+    this.prenom = localStorage.getItem('prenom');
+    this.nom = localStorage.getItem('nom');
+    this.matricule = localStorage.getItem('matricule');
+  }
+  deconnexion()
+  {
+    // Effacer tous les éléments
+    localStorage.removeItem('token');
+    localStorage.removeItem('id');
+    localStorage.removeItem('prenom');
+    localStorage.removeItem('nom');
+    localStorage.removeItem('matricule');
   }
 }
