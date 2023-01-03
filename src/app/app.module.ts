b0bfb798a4ae364dd70fe0c81bb7dd768a77ea73
Josephine <sosodjibrildiop@gmail.com>
2023-01-03 18:14:56 +0000

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageAdminComponent } from './page-admin/page-admin.component';
+import { ListeUserComponent } from './liste-user/liste-user.component';
+import { PageUserComponent } from './page-user/page-user.component';
+import { ListeAdministrateurComponent } from './liste-administrateur/liste-administrateur.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAdminComponent,
+    ListeUserComponent,
+    PageUserComponent,
+    ListeAdministrateurComponent,
+   
+  
+   
   ],
   imports: [
     BrowserModule,
