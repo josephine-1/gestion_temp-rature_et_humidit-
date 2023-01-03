29f94845cd0a6cb1fd7edda2424d7d427fa4eae1
bmb <bmb>
2023-01-03 16:20:23 +0000

    front-end dashbord

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent} from './dashbord/dashbord.component';
+import { DashbordOnComponent} from './dashbord-on/dashbord-on.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"off", component: DashbordComponent},
+  {path: 'on', component: DashbordOnComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { DashbordOnComponent } from './dashbord-on/dashbord-on.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbordComponent,
+    DashbordOnComponent
   ],
   imports: [
     BrowserModule,
