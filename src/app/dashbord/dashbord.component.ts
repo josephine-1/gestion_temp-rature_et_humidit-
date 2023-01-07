e89e73969b57a170e6a6527527d0658954f5fe0e
bmb <bmb>
2023-01-07 18:18:50 +0000

    cool

--- src/app/dashbord/dashbord.component.ts
+++ src/app/dashbord/dashbord.component.ts
@@ -1,33 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, TRANSLATIONS } from '@angular/core';
 
 @Component({
   selector: 'app-dashbord',
   templateUrl: './dashbord.component.html',
   styleUrls: ['./dashbord.component.scss']
 })
+
 export class DashbordComponent {
+
   imageSrc = 'assets/fanstop.png';
-  not = '';
-  on = '';
-  off = '';
-  imageNot= [ {src:'assets/noton.jpg'}]
+  onSrc = 'assets/noton.jpg';
+  offSrc = 'assets/off.jpg' ;
 
-  imageOn= [ {src:'assets/on.jpg'}]
+  imageOn= [ {srco:'assets/on.jpg'}];
+  imageButtons = [ {src:'assets/fanstop.png', srcs:'assets/noton.jpg', srcr :'assets/off.jpg'}];
+  imageButtonOn = [ {src:'assets/fanrun.gif',  srcr:'assets/notoff.jpg', srcs : 'assets/on.jpg'}];
 
-  imageOff= [ {src:'assets/off.jpg'}]
-
-  imageButtons = [ {src:'assets/fanstop.png', }]
-  imageButtonOn = [ {src:'assets/fanrun.gif', }]
 constructor() { }
   ngOnInit() {
-    
-  }
-  onClick(imageNameObject: { src: string;  }) {
-    this.imageSrc = imageNameObject.src;
+
   }
 
-onClickon(imageNameObject: { src: string;  }) {
-  this.imageSrc = imageNameObject.src;
-}
+
+  onClick(imageNameObject: {
+    srcr: string;
+    srcs: string; src: string;
+}) {
+    this.imageSrc = imageNameObject.src;
+    this.onSrc = imageNameObject.srcs;
+    this.offSrc = imageNameObject.srcr;
+  }
 
 }
