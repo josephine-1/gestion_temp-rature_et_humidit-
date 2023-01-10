3d7a90daf28bf4272cecca6d924151d2c492f019
issa <i.ndiaye@isepdiamniadio.edu.sn>
2023-01-10 17:21:02 +0000

    first commit

--- src/app/liste-administrateur/liste-administrateur.component.ts
+++ src/app/liste-administrateur/liste-administrateur.component.ts
@@ -36,7 +36,9 @@ export class ListeAdministrateurComponent implements OnInit {
 
  import { Component,OnInit } from '@angular/core';
 import UsersJson from '../users.json';
-
+import { AuthService } from '../shared/auth.service';
+import { CrudService } from '../services/inscription.service';
+import { Ng2SearchPipeModule } from 'ng2-search-filter'; /* recherche */
 
 interface USERS {
   
@@ -54,31 +56,67 @@ interface USERS {
 })
 export class ListeAdministrateurComponent implements OnInit{
 
-  p:number=1;
-  searchText!:string;
-  Users: USERS[] = UsersJson;
+   /* liste fiective à remplacer Books par []  pour les données rééels*/
 
-  prenom!:any;
-  nom!:any;
-  matricule!:any
-  etat:any = localStorage.getItem('token');
+  pages: number = 1;
+  searchText:any; // search installer npm i ng2-search-filter
 
-  constructor(){
-    console.log(this.Users);
-  }
+  User: any = [];
+  data:any;
+
+  constructor(public AuthService: AuthService) {}
+
   ngOnInit(): void {
-    this.prenom = localStorage.getItem('prenom');
-    this.nom = localStorage.getItem('nom');
-    this.matricule = localStorage.getItem('matricule');
-  }
-  deconnexion()
-  {
-   
-    localStorage.removeItem('token');
-    localStorage.removeItem('id');
-    localStorage.removeItem('prenom');
-    localStorage.removeItem('nom');
-    localStorage.removeItem('matricule');
-  }
+    this.AuthService.getAllUser().subscribe((res) => {
+      console.log(res);
+      this.data = res;
+      this.User = this.data.filter((e: any) => e.etat == true );
+
+      /*
+      ngOnInit(): void {
+
+  this.userService.getUsers().subscribe(
+      data =>{
+
+        this.users = data;
+
+        this.crudService = this.Books.filter((e:any)=> e.etat == true)
+        console.log(this.userActif)
+      }
+);
+
 }
+      */
+    });
+  }
+
+/*   delete(id: any, i: any) {
+    console.log(id);
+    if (window.confirm('Do you want to go ahead?')) {
+      this.AuthService.deleteBook(id).subscribe((res) => {
+        this.Books.splice(i, 1);
+      });
+    }
+  } */
+//  changeRole = (id: any, role: any) => {
+  //  role = role == 'administrateur' ? 'user_simple' : 'administrateur'; /* pour switche */
+ /*    const user = { profil: role };
+    if (confirm('Changer de role')) {
+      this.AuthService.change_role(id, user).subscribe((data) => {
+        this.ngOnInit();
+      });
+    }
+  }; */
+
+ // changeRole2 = (id: any, etat: any) => {
+   // etat = etat == true ? false : true; /* pour archiver */
+    /* const user = { etat: etat };
+    if (confirm('Archiver !!!')) {
+      this.crudService.change_role(id, user).subscribe((data) => {
+        this.ngOnInit();
+      });
+    }
+  }; */
+}
+
  
