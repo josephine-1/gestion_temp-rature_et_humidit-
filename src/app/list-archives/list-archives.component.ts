bad02d4ca55d6985dc85fed44a70e20424d5733f
Josephine <sosodjibrildiop@gmail.com>
2023-01-10 16:22:49 +0000

    page_admin

--- src/app/list-archives/list-archives.component.ts
+++ src/app/list-archives/list-archives.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/* import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-archives',
@@ -8,3 +8,52 @@ import { Component } from '@angular/core';
 export class ListArchivesComponent {
 
 }
+ */
+import { Component,OnInit } from '@angular/core';
+import UsersJson from '../users.json';
+
+
+interface USERS {
+  
+  Nom: String;
+  Prenom: String;
+  Matricule: String;
+  Email: String;
+  Date_inscription: String;
+}
+
+@Component({
+  selector: 'app-list-archives',
+  templateUrl: './list-archives.component.html',
+  styleUrls: ['./list-archives.component.scss']
+})
+export class ListArchivesComponent implements OnInit{
+
+  p:number=1;
+  searchText!:string;
+  Users: USERS[] = UsersJson;
+
+  prenom!:any;
+  nom!:any;
+  matricule!:any
+  etat:any = localStorage.getItem('token');
+
+  constructor(){
+    console.log(this.Users);
+  }
+  ngOnInit(): void {
+    this.prenom = localStorage.getItem('prenom');
+    this.nom = localStorage.getItem('nom');
+    this.matricule = localStorage.getItem('matricule');
+  }
+  deconnexion()
+  {
+   
+    localStorage.removeItem('token');
+    localStorage.removeItem('id');
+    localStorage.removeItem('prenom');
+    localStorage.removeItem('nom');
+    localStorage.removeItem('matricule');
+  }
+}
+ 
