00db8c3a40ac70ea9f8ecc7855709a2464814434
Josephine <sosodjibrildiop@gmail.com>
2023-01-11 12:19:45 +0000

    pull

--- src/app/liste-administrateur/liste-administrateur.component.ts
+++ src/app/liste-administrateur/liste-administrateur.component.ts
@@ -1,37 +1,4 @@
-/* import { Component, NgZone, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import Swal from 'sweetalert2';
-@Component({
-  selector: 'app-liste-administrateur',
-  templateUrl: './liste-administrateur.component.html',
-  styleUrls: ['./liste-administrateur.component.scss']
-})
-export class ListeAdministrateurComponent implements OnInit {
-  constructor(private ngZone:NgZone,private router: Router,private activatedRoute: ActivatedRoute
-    ){}
 
-  deconnect(){
-    Swal.fire({
-      title: 'Déconnexion',
-      text: 'Êtes-vous sûre de vouloir vous déconnecter ?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'OUI',
-      cancelButtonText: 'NON',
-    }).then((result) => {
-        if (result.value) {
-          this.ngZone.run(() => this.router.navigateByUrl('/'));
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-          (result.dismiss === Swal.DismissReason.cancel)
-        }
-    })
-  }
-
-  ngOnInit(): void {
-
-  }
-}
- */
 
 
  import { Component,OnInit } from '@angular/core';
