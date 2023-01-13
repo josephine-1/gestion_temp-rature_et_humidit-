7d878faaa0927847379872555a9d877b9481136e
Josephine <sosodjibrildiop@gmail.com>
2023-01-13 15:32:55 +0000

    enre

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,14 +26,7 @@ constructor(
 
   ngOnInit(): void {
     this.signinForm = this.fb.group({
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.email,
-          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-        ],
-      ],
+      email : ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password : ['', Validators.required],
     });
   }
