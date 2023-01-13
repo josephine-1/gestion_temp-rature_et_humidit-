d882080fa0254cc737c92625dcd8202e2a10ffd0
Josephine <sosodjibrildiop@gmail.com>
2023-01-13 12:45:41 +0000

    controle

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -45,7 +45,7 @@ export class InscriptionComponent implements OnInit {
           '',
           [
             Validators.required,
-            Validators.email,
+            Validators.email,noWhitespaceValidator,
             Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
           ],
         ],
